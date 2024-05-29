c5f4bef177b79c2d1b67736b1c74f10af77b51c7
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-05-29 09:15:57 +0200

    Update moon-phase_1.ps1

--- moon-phase/moon-phase_1.ps1
+++ moon-phase/moon-phase_1.ps1
@@ -1,5 +1,4 @@
 ﻿<#
-
 #!/bin/bash
 moonphase(){
   local lp=2551443
@@ -9,9 +8,7 @@ moonphase(){
   local newmoon=592500
   local phase=$((($now - $newmoon) % $lp))
   local phase_number=$((((phase / 86400) + 1)*100000))
-
   # Multiply by 100000 so we can do integer comparison.  Go Bash!
-
   if   [ $phase_number -lt 184566 ];  then phase_icon="new"
   elif [ $phase_number -lt 553699 ];  then phase_icon="waxing crescent"
   elif [ $phase_number -lt 922831 ];  then phase_icon="first quarter"
@@ -25,7 +22,6 @@ moonphase(){
   fi
   echo $phase_icon
 }
-
 moonphase
 #>
 
